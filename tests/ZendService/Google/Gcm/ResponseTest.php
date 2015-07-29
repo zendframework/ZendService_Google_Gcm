b31eb24bb4bec3643c654c5be95ef7d520d95b1b
John Boehr <jbboehr@gmail.com>
2015-07-29 16:52:48 -0700

    Update phpunit to 4.7 for PHP7 compatibility

--- tests/ZendService/Google/Gcm/ResponseTest.php
+++ tests/ZendService/Google/Gcm/ResponseTest.php
@@ -58,19 +58,12 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
 
     public function testInvalidConstructorThrowsException()
     {
-        // phpunit doesn't catch BaseException in PHP7
         if( PHP_VERSION_ID >= 70000 ) {
-            $e = null;
-            try {
-                $response = new Response('{bad');
-            } catch ( \EngineException $ex ) {
-                $e = $ex;
-            }
-            $this->assertInstanceOf('\\TypeException', $e);
+            $this->setExpectedException('TypeError');
         } else {
             $this->setExpectedException('PHPUnit_Framework_Error');
-            $response = new Response('{bad');
         }
+        $response = new Response('{bad');
     }
 
     public function testMessageExpectedBehavior()
