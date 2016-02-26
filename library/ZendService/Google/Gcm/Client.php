8a424c820f72eacccd1dd096b01c1e9b6929fdb7
Samuel Ventimiglia <sviluppo@tulain.com>
2016-02-26 12:44:30 +0100

    TO RESOLV ERROR
    Unable to enable crypto on TCP connection gcm-http.googleapis.com: make
    sure the "sslcafile" or "sslcapath" option are properly set for the
    environment.
    
     ON SOME SERVER.
    
    Signed-off-by: Samuel Ventimiglia <sviluppo@tulain.com>

--- library/ZendService/Google/Gcm/Client.php
+++ library/ZendService/Google/Gcm/Client.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -8,7 +9,6 @@
  * @category  ZendService
  * @package   ZendService_Google\Gcm
  */
-
 namespace ZendService\Google\Gcm;
 
 use ZendService\Google\Exception;
@@ -20,122 +20,128 @@ use Zend\Json\Json;
  * This class allows the ability to send out messages
  * through the Google Cloud Messaging API.
  *
- * @category   ZendService
- * @package    ZendService_Google
+ * @category ZendService
+ * @package ZendService_Google
  * @subpackage Gcm
  */
-class Client
-{
-    /**
-     * @const string Server URI
-     */
-    const SERVER_URI = 'https://gcm-http.googleapis.com/gcm/send';
-
-    /**
-     * @var Zend\Http\Client
-     */
-    protected $httpClient;
-
-    /**
-     * @var string
-     */
-    protected $apiKey;
-
-    /**
-     * Get API Key
-     *
-     * @return string
-     */
-    public function getApiKey()
-    {
-        return $this->apiKey;
-    }
-
-    /**
-     * Set API Key
-     *
-     * @param string $apiKey
-     * @return Client
-     * @throws InvalidArgumentException
-     */
-    public function setApiKey($apiKey)
-    {
-        if (!is_string($apiKey) || empty($apiKey)) {
-            throw new Exception\InvalidArgumentException('The api key must be a string and not empty');
-        }
-        $this->apiKey = $apiKey;
-        return $this;
-    }
-
-    /**
-     * Get HTTP Client
-     *
-     * @return Zend\Http\Client
-     */
-    public function getHttpClient()
-    {
-        if (!$this->httpClient) {
-            $this->httpClient = new HttpClient();
-            $this->httpClient->setOptions(array('strictredirects' => true));
-        }
-        return $this->httpClient;
-    }
-
-    /**
-     * Set HTTP Client
-     *
-     * @param Zend\Http\Client
-     * @return Client
-     */
-    public function setHttpClient(HttpClient $http)
-    {
-        $this->httpClient = $http;
-        return $this;
-    }
-
-    /**
-     * Send Message
-     *
-     * @param Mesage $message
-     * @return Response
-     * @throws Exception\RuntimeException
-     */
-    public function send(Message $message)
-    {
-        $client = $this->getHttpClient();
-        $client->setUri(self::SERVER_URI);
-        $headers = $client->getRequest()->getHeaders();
-        $headers->addHeaderLine('Authorization', 'key=' . $this->getApiKey());
-
-        $response = $client->setHeaders($headers)
-                           ->setMethod('POST')
-                           ->setRawBody($message->toJson())
-                           ->setEncType('application/json')
-                           ->send();
-
-        switch ($response->getStatusCode()) {
-            case 500:
-                throw new Exception\RuntimeException('500 Internal Server Error');
-                break;
-            case 503:
-                $exceptionMessage = '503 Server Unavailable';
-                if ($retry = $response->getHeaders()->get('Retry-After')) {
-                    $exceptionMessage .= '; Retry After: ' . $retry;
-                }
-                throw new Exception\RuntimeException($exceptionMessage);
-                break;
-            case 401:
-                throw new Exception\RuntimeException('401 Forbidden; Authentication Error');
-                break;
-            case 400:
-                throw new Exception\RuntimeException('400 Bad Request; invalid message');
-                break;
-        }
-
-        if (!$response = Json::decode($response->getBody(), Json::TYPE_ARRAY)) {
-            throw new Exception\RuntimeException('Response body did not contain a valid JSON response');
-        }
-
-        return new Response($response, $message);
-    }
+class Client {
+	/**
+	 * @const string Server URI
+	 */
+	const SERVER_URI = 'https://gcm-http.googleapis.com/gcm/send';
+	
+	/**
+	 *
+	 * @var Zend\Http\Client
+	 */
+	protected $httpClient;
+	
+	/**
+	 *
+	 * @var string
+	 */
+	protected $apiKey;
+	
+	/**
+	 * Get API Key
+	 *
+	 * @return string
+	 */
+	public function getApiKey() {
+		return $this->apiKey;
+	}
+	
+	/**
+	 * Set API Key
+	 *
+	 * @param string $apiKey        	
+	 * @return Client
+	 * @throws InvalidArgumentException
+	 */
+	public function setApiKey($apiKey) {
+		if (! is_string ( $apiKey ) || empty ( $apiKey )) {
+			throw new Exception\InvalidArgumentException ( 'The api key must be a string and not empty' );
+		}
+		$this->apiKey = $apiKey;
+		return $this;
+	}
+	
+	/**
+	 * Get HTTP Client
+	 *
+	 * @return Zend\Http\Client
+	 */
+	public function getHttpClient() {
+		if (! $this->httpClient) {
+			$this->httpClient = new HttpClient ();
+			$this->httpClient->setOptions ( array (
+					'strictredirects' => true,
+					/**
+					 *
+					 * @author Ventimiglia Samuel
+					 * @since 26-02-2015
+					 * @filesource Added this line becouse some server return
+					 *            
+					 *             ===Unable to enable crypto on TCP connection gcm-http.googleapis.com: make sure the "sslcafile" or "sslcapath"
+					 *             option are properly set for the environment.======
+					 *             --------------------------------------
+					 */
+					'sslcapath' => '/etc/ssl/certs' 
+			) );
+		}
+		return $this->httpClient;
+	}
+	
+	/**
+	 * Set HTTP Client
+	 *
+	 * @param
+	 *        	Zend\Http\Client
+	 * @return Client
+	 */
+	public function setHttpClient(HttpClient $http) {
+		$this->httpClient = $http;
+		return $this;
+	}
+	
+	/**
+	 * Send Message
+	 *
+	 * @param Mesage $message        	
+	 * @return Response
+	 * @throws Exception\RuntimeException
+	 */
+	public function send(Message $message) {
+		$client = $this->getHttpClient ();
+		$client->setUri ( self::SERVER_URI );
+		$headers = $client->getRequest ()->getHeaders ();
+		$headers->addHeaderLine ( 'Authorization', 'key=' . $this->getApiKey () );
+		
+		$response = $client->setHeaders ( $headers )->setMethod ( 'POST' )->setRawBody ( $message->toJson () )->setEncType ( 'application/json' )->send ();
+		
+		switch ($response->getStatusCode ()) {
+			case 500 :
+				throw new Exception\RuntimeException ( '500 Internal Server Error' );
+				break;
+			case 503 :
+				$exceptionMessage = '503 Server Unavailable';
+				if ($retry = $response->getHeaders ()->get ( 'Retry-After' )) {
+					$exceptionMessage .= '; Retry After: ' . $retry;
+				}
+				throw new Exception\RuntimeException ( $exceptionMessage );
+				break;
+			case 401 :
+				throw new Exception\RuntimeException ( '401 Forbidden; Authentication Error' );
+				break;
+			case 400 :
+				throw new Exception\RuntimeException ( '400 Bad Request; invalid message' );
+				break;
+		}
+		
+		if (! $response = Json::decode ( $response->getBody (), Json::TYPE_ARRAY )) {
+			throw new Exception\RuntimeException ( 'Response body did not contain a valid JSON response' );
+		}
+		
+		return new Response ( $response, $message );
+	}
 }
